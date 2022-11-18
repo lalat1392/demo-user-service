b3698732d2ef953af43d83f9e29ddbad43f9e572
lalata.sahoo <lalatsahoo.92@gmail.com>
2022-11-18 15:57:19 +0530

    java version downgrade 11 to 1.8

--- src/main/java/com/example/demo/exception/NotFoundException.java
+++ src/main/java/com/example/demo/exception/NotFoundException.java
@@ -1,5 +1,9 @@
 package com.example.demo.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason="User Not Found")
 public class NotFoundException extends RuntimeException {
     private Integer errorCode;
 
